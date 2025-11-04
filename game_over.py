172c2ec90fee81a2fc946a699f0f48d52e75012f
elsa <230103033@mhs.udb.ac.id>
2025-11-04 19:52:52 +0700

    ketika game over awal

---
+++ game_over.py
@@ -0,0 +1,14 @@
+import pygame 
+ 
+# --- Variabel Lokal untuk Game Over --- 
+BLACK = (0, 0, 0) 
+WHITE = (255, 255, 255) 
+RED = (200, 0, 0)  
+GREEN = (0, 150, 0)  
+ 
+SCREEN_WIDTH = 800 
+SCREEN_HEIGHT = 600 
+ 
+# Inisialisasi font  
+title_font = pygame.font.SysFont(None, 100) 
+button_font = pygame.font.SysFont(None, 45) 
