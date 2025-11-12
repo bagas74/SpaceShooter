c080de741dbc4e7fa120d4d5637fb03a394c9a78
Adit <adhityaputramahendra01@gmail.com>
2025-11-12 13:19:24 +0700

    Inisialisasi font

--- menu.py
+++ menu.py
@@ -6,3 +6,7 @@ GREEN_TOSCA = (0, 204, 153)
 # Kita perlu tahu ukuran layar untuk menempatkan tombol
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
+# Inisialisasi font (Pygame sudah di-init di main.py)
+title_font = pygame.font.SysFont(None, 80)
+button_font = pygame.font.SysFont(None, 50)
